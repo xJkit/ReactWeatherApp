59c9a7f98f1c9040d4e784152a35831eb7643646
joey-mbpr <xjkit54780@gmail.com>
2016-10-06 14:27:54 +0800

    Add the About page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,20 @@ import React from 'react';
 let About =  (props) => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1 className="text-center">關於</h1>
+      <p>這是一個使用 React 開發的天氣查詢網站</p>
+      <p>相關技術如下：</p>
+      <ul className="">
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> | A JavaScript Library for Building UI
+        </li>
+        <li>
+          <a href="https://github.com/ReactTraining/react-router">react-router</a> | Declarative routing for React. Essential for Single Page Applications.
+        </li>
+        <li>
+          <a href="https://github.com/mzabriskie/axios">axios</a> | Promise-based HTTP client for the browser and node.js.
+        </li>
+      </ul>
     </div>
   );
 };
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const WeatherMessage = (props) => {
   let {cityName: city, cityTemp: temp, cityLon: lon, cityLat: lat} = props;
   return(
-    <h3>{city} is {temp} deg located at {lon}, {lat}</h3>
+    <div>
+      <ul className="menu vertical">
+        <li>城市：{city}</li>
+        <li>溫度：{temp}</li>
+        <li>經度：{lon}</li>
+        <li>緯度：{lat}</li>
+      </ul>
+    </div>
   )
 }
 module.exports = WeatherMessage;
